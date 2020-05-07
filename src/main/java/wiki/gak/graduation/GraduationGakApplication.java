c09b9cacbf3910354a311158920e6e8124c14845
EchoCow <lizhongyue248@163.com>
2020-05-08 01:36:56 +0800

    :tada: Initial commit.

---
+++ src/main/java/wiki/gak/graduation/GraduationGakApplication.java
@@ -0,0 +1,18 @@
+package wiki.gak.graduation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Graduation Application
+ *
+ * @author echo
+ */
+@SpringBootApplication
+public class GraduationGakApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GraduationGakApplication.class, args);
+  }
+
+}
---
+++ src/test/java/wiki/gak/graduation/GraduationGakApplicationTests.java
@@ -0,0 +1,13 @@
+package wiki.gak.graduation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduationGakApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
